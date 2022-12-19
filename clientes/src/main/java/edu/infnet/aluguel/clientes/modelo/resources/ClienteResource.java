eec6b8b95f52f00138c32cd73eedbb438934aa27
Eduardo De Lucca <eduardo.luccati@gmail.com>
2022-12-19 20:25:25 -0300

    Ajuste - Swagger (Aluguel)

--- clientes/src/main/java/edu/infnet/aluguel/clientes/modelo/resources/ClienteResource.java
+++ clientes/src/main/java/edu/infnet/aluguel/clientes/modelo/resources/ClienteResource.java
@@ -2,24 +2,28 @@ package edu.infnet.aluguel.clientes.modelo.resources;
 
 import edu.infnet.aluguel.clientes.modelo.entidades.Cliente;
 import edu.infnet.aluguel.clientes.modelo.services.ClienteService;
+
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/clientes")
+@EnableAutoConfiguration
 public class ClienteResource {
 
-    private static Logger log = LoggerFactory.getLogger(ClienteResource.class);
+    private static final Logger log = LoggerFactory.getLogger(ClienteResource.class);
 
     @Autowired
     private ClienteService clienteService;
 
-    @GetMapping("/{codigo}")
+
+    @GetMapping("/clientes/{codigo}")
     public Cliente getCliente(@PathVariable Long codigo) {
         log.info("Chamada da API de cliente com código: [" + codigo + "]");
 
